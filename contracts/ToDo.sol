238d4968f200a86459fcaa1d0f73b061259de9cc
Quantaf1re <37821211+quantaf1re@users.noreply.github.com>
2020-11-24 15:55:46 +0100

    Initial contract and brownie init

---
+++ contracts/ToDo.sol
@@ -0,0 +1,42 @@
+pragma solidity >=0.7.0 <0.8.0;
+
+contract ToDo {
+
+    address public owner;
+    uint public taskCount = 0;
+    mapping(uint => Task) public tasks;
+
+    struct Task {
+        uint id;
+        string description;
+        Status status;
+    }
+
+    enum Status { Created, InProgress, Completed, Cancelled }
+    event ModTask(uint indexed id, Status newStatus);
+
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function createTask(string memory _description) external onlyOwner {
+        // Technically this could overflow, but perhaps that's desirable
+        // behaviour - you probably don't care about overwriting a task
+        // from 2^256 tasks ago and don't want to redeploy a contract
+        // that must have been working well for that long
+        tasks[taskCount] = Task(taskCount, _description, Status.Created);
+        taskCount++;
+        emit ModTask(taskCount, Status.Created);
+    }
+
+    function modTask(uint _id, Status _newStatus) external onlyOwner {
+        tasks[_id].status = _newStatus;
+        emit ModTask(_id, _newStatus);
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Not the owner");
+        _;
+    }
+}
